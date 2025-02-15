654c175c5ea21abdcdd342a378f1f655526e10d7
surin.i <surin.i@skbkontur.ru>
2025-02-15 14:52:15 +0500

    BaseDataStructures

---
+++ Lessons/src/BaseDataStructures/MinStack.java
@@ -0,0 +1,45 @@
+﻿package BaseDataStructures;
+
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minsStack;
+
+    public MinStack()
+    {
+        stack = new Stack<Integer>();
+        minsStack = new Stack<Integer>();
+    }
+
+    public void push(int val)
+    {
+        stack.push(val);
+
+        if(minsStack.empty() || minsStack.peek() >= val)
+        {
+            minsStack.push(val);
+        }
+    }
+
+    public void pop()
+    {
+        if(stack.peek().equals(minsStack.peek()))
+        {
+            minsStack.pop();
+        }
+
+        stack.pop();
+    }
+
+    public int top()
+    {
+        return stack.peek();
+    }
+
+    public int getMin()
+    {
+        return minsStack.peek();
+    }
+}
